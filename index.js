c70f68e7b56f588ed5c3c31f3c4a8a72f866d5cd
paulstgermain <paulg.stgermain@gmail.com>
2021-03-11 13:31:18 -0500

    serves up react app from express server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const path = require('path');
+
 console.log('Web 39 RULES. Big time.');
 
 const express = require('express');
@@ -7,6 +9,7 @@ const express = require('express');
 const server = express();
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build')));
 
 const port = process.env.PORT;
 
@@ -15,9 +18,15 @@ if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env varia
     server.use(cors());
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>SUCCESS</h1>');
-})
+// our API comes earlier in the pipeline
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'HELLO' });
+});
+
+// catch all that just sends back index.html -> our react app
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 server.listen(port, () => {
     console.log(`Magic happening on Port:${port}`)
