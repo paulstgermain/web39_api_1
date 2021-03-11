3521ca999e8b6bbcd1dd1dd4fa7d2c624c93d943
paulstgermain <paulg.stgermain@gmail.com>
2021-03-11 13:58:02 -0500

    adds ability to tap into API from react app served by express

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getUrl(path) {
+  if(process.env.NODE_ENV === 'development') {
+    return 'http://localhost:5000' + path
+  } else {
+    return path
+  }
+}
+
 function App() {
+  const [message, setMessage] = useState('Hello Web39');
+
+  useEffect(() => {
+    fetch(getUrl('/api/hello'))
+    .then(res => res.json())
+    .then(resBody => setMessage(resBody.message))
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {message}
       </header>
     </div>
   );
