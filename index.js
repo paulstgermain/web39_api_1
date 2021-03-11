3817083995ae862d53be567464061cd6d669fac7
paulstgermain <paulg.stgermain@gmail.com>
2021-03-11 12:48:23 -0500

    adds env variables

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+require('dotenv').config();
+
 console.log('Web 39 RULES. Big time.');
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const port = process.env.PORT;
+
+if (process.env.NODE_ENV === 'development') { // on Heroku machine, an env variable is called "NODE_ENV" -> production
+    const cors = require('cors');
+    server.use(cors());
+}
+
+server.listen(port, () => {
+    console.log(`Magic happening on Port:${port}`)
+})
